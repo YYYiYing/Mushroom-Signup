20030023e73675ab94f00740d31f7205798a2f4b
YYYiYing <secretsoulful@gmail.com>
2025-04-04 21:59:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('mushroomForm').addEventListener('submit', function(even
   event.preventDefault(); // 阻止表單預設的提交行為
   var formData = new FormData(this);
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', '您的 Google Apps Script 應用程式 URL');
+  xhr.open('POST', 'https://script.google.com/macros/s/AKfycbxqIhv6HGkrqF9-BV1L8y0L_ctTxeKnU7vMzB3EqkjKryv3mLZbszsaa-b0jIfjTpU1ww/exec');
   xhr.send(formData);
   xhr.onload = function() {
     alert(xhr.responseText); // 顯示 Google Apps Script 回傳的訊息
